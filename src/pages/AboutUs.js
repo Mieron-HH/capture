06e8f5efa32656fa5b443e20700eaaafb60b20e3
NeverLost-Never <haileyesus.mieron@gmail.com>
2022-01-13 16:59:34 +0400

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //Page Components
 import AboutSection from "../component/AboutSection";
 import ServicesSection from "../component/ServicesSection";
+import FaqSection from "../component/FaqSection";
 
 const AboutUs = () => {
 	return (
 		<>
 			<AboutSection />
 			<ServicesSection />
+			<FaqSection />
 		</>
 	);
 };
