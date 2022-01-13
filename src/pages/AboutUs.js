e0688e9d86b38d2932910b25f527f692df84a35f
NeverLost-Never <haileyesus.mieron@gmail.com>
2022-01-13 16:40:14 +0400

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+//Page Components
+import AboutSection from "../component/AboutSection";
+
+const AboutUs = () => {
+	return <AboutSection />;
+};
+
+export default AboutUs;
