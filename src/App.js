7431387e0cde4374d0f1241d46fa7a8ac0b238a2
NeverLost-Never <haileyesus.mieron@gmail.com>
2022-01-15 19:27:45 +0400

    5. intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//Global Styles
+import GlobalStyle from "./component/GlobalStyle";
 //Import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
 	return (
 		<div className="App">
+			<GlobalStyle />
 			<AboutUs />
 		</div>
 	);
