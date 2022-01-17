8730fcc615cdca1e9d21b3eb3aeaccc75e559b49
NeverLost-Never <haileyesus.mieron@gmail.com>
2022-01-17 17:34:20 +0400

    12. Page Transitions

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,14 +3,22 @@ import React from "react";
 import AboutSection from "../component/AboutSection";
 import ServicesSection from "../component/ServicesSection";
 import FaqSection from "../component/FaqSection";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const AboutUs = () => {
 	return (
-		<>
+		<motion.div
+			variants={pageAnimation}
+			initial="hidden"
+			animate="show"
+			exit="exit"
+		>
 			<AboutSection />
 			<ServicesSection />
 			<FaqSection />
-		</>
+		</motion.div>
 	);
 };
 
